1ca453a2dd79e0890eda50f9994cf81276a5f567
Elias33 <elias35-1426@diu.edu.bd>
2017-08-26 05:09:31 -0700

    Factorial number

---
+++ src/factorial/main.java
@@ -0,0 +1,11 @@
+package factorial;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pkg obj= new pkg();
+		obj.fact(10);
+	}
+
+}
---
+++ src/factorial/pkg.java
@@ -0,0 +1,17 @@
+package factorial;
+
+public class pkg {
+public void fact(int n) {
+		
+
+		int factorial = 0;
+		
+		
+			for(int i=0;i<=n;i++){
+				 factorial=factorial*i;
+			}
+			System.out.println(""+ factorial);
+	
+              
+	}    
+}
